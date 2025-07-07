79e9c4b65c8377cbd07bfb7980e31567164b6828
AbrJA <ajaimes@occ.com.mx>
2025-07-07 16:19:51 -0600

    Fix function calls in runtests.jl to use bang (!) versions for consistency

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,13 +3,12 @@ using Test
 
 @testset "Mongoose.jl" begin
     function greet(conn, request)
-        # @info mg_body(request)
         mg_json_reply(conn, 200, json)
     end
 
-    mg_register("GET", "/hello", greet)
+    mg_register!("GET", "/hello", greet)
 
-    mg_serve()
+    mg_serve!()
     sleep(10)
-    mg_shutdown()
+    mg_shutdown!()
 end
